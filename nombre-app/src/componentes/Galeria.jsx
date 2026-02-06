8fd8bce86683c7686c577af0e466671a59d7f4b2
Bandhack <240852@utxicotepec.edu.mx>
2026-02-06 08:59:35 -0600

    Contenido Agregado Al Resto De Botones Actualizados

--- nombre-app/src/componentes/Galeria.jsx
+++ nombre-app/src/componentes/Galeria.jsx
@@ -1,3 +1,7 @@
+import GeometryDash1 from "../assets/GeometryDash1.png";
+import CODMobile from "../assets/CODMobile.png";
+import BloodStrikeEmma from "../assets/BloodStrikeEmma.png";
+
 function Galeria() {
     return (
         <div className="acerca-contenedor">
@@ -6,6 +10,9 @@ function Galeria() {
                 Aquí se presentan imágenes y recursos visuales de los proyectos
                 realizados durante la carrera de Desarrollo de Software.
             </p>
+                <img src={BloodStrikeEmma} />
+                <img src={GeometryDash1} />
+                <img src={CODMobile} />
         </div>
     );
 }
--- nombre-app/src/componentes/Productos.jsx
+++ nombre-app/src/componentes/Productos.jsx
@@ -1,3 +1,7 @@
+import GeometryDash1 from "../assets/GeometryDash1.png";
+import CODMobile from "../assets/CODMobile.png";
+import BloodStrikeEmma from "../assets/BloodStrikeEmma.png";
+
 function Productos() {
     return (
         <div className="acerca-contenedor">
@@ -6,6 +10,9 @@ function Productos() {
                 En esta sección se mostrarán los productos desarrollados como
                 proyectos académicos, prácticas y demostraciones en React.
             </p>
+                <img src={GeometryDash1} />
+                <img src={CODMobile} />
+                <img src={BloodStrikeEmma} />
         </div>
     );
 }
